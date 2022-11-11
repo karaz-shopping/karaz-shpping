cb915daccc6d6c338ed7295e83a87f0b08526367
Sohib Al-Bakri <84742021+SohibAl-Bakri@users.noreply.github.com>
2022-11-11 19:54:08 +0300

    solve the error

--- lib/pages/home/home_page/custom_home.dart
+++ lib/pages/home/home_page/custom_home.dart
@@ -30,60 +30,65 @@ class _CustomHomeState extends State<CustomHome> {
         // ),
         width: double.infinity,
         height: double.infinity,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width - 40,
-                height: 46,
-                child: const TextField(
-                  decoration: InputDecoration(
-                    label: Text('Search'),
-                    icon: Icon(
-                      Icons.search_rounded,
-                    ),
-
-                    // suffixIcon: Visibility(
-                    //   visible: change,
-                    //   child: IconButton(
-                    //     icon: Icon(Icons.highlight_off_rounded),
-                    //     onPressed: () {
-                    //       setState(() {
-                    //         searchController.clear();
-                    //         change = false;
-                    //       });
-                    //     },
-                    //   ),
-                    // ),
+        child: ListView(
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width - 40,
+              height: 46,
+              child: const TextField(
+                decoration: InputDecoration(
+                  label: Text('Search'),
+                  icon: Icon(
+                    Icons.search_rounded,
                   ),
-                  // controller: searchController,
-                  // onChanged: (value) {
-                  //   setState(() {
-                  //     if (value == '') {
-                  //       change = false;
-                  //     } else {
-                  //       change = true;
-                  //     }
-                  //     search = value;
-                  //   });
-                  // },
+
+                  // suffixIcon: Visibility(
+                  //   visible: change,
+                  //   child: IconButton(
+                  //     icon: Icon(Icons.highlight_off_rounded),
+                  //     onPressed: () {
+                  //       setState(() {
+                  //         searchController.clear();
+                  //         change = false;
+                  //       });
+                  //     },
+                  //   ),
+                  // ),
                 ),
+                // controller: searchController,
+                // onChanged: (value) {
+                //   setState(() {
+                //     if (value == '') {
+                //       change = false;
+                //     } else {
+                //       change = true;
+                //     }
+                //     search = value;
+                //   });
+                // },
               ),
-              const CustomCircleAvatar(),
-              const CustomSlider(),
-              const CustomAdsCard(),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(
-                    builder: (context) {
-                      return const ViewProduct();
-                    },
-                  ));
-                },
-                child: const Text('view data'),
-              ),
-            ],
-          ),
+            ),
+            const CustomCircleAvatar(),
+            const CustomSlider(),
+            const CustomAdsCard(),
+            // Container(
+            //   width: double.infinity,
+            //   height: 500,
+            //   color: Colors.blueAccent,
+            //   child: const CustomAdsCard(),
+            // ),
+            //const CustomAdsCard(),
+            // ElevatedButton(
+            //   onPressed: () {
+            //     Navigator.push(context, MaterialPageRoute(
+            //       builder: (context) {
+            //         return const ViewProduct();
+            //       },
+            //     ));
+            //   },
+            //   child: const Text('view data'),
+            // ),
+          ],
         ),
       ),
     );
